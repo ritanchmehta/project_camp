d4e563f45ea666ed3156ad485bce6676ef47ab34
ritanchmehta <ritanch0001@gmail.com>
2025-08-14 00:16:11 +0530

    added healthcheck

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 
+
 const app = express();
 
 //basic configurations
@@ -15,6 +16,11 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+//import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
 
 app.get("/", (req, res) => {
   res.send("hello");
